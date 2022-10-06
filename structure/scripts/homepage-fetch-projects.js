481b4fc3f434ed4b53fb3c4c761613dcc2083a64
Enric <enricorillosa@hotmail.es>
2022-10-06 21:35:42 +0200

    Updated 19
    Homepage JS fetch petitions finished with exit.

--- structure/scripts/homepage-fetch-projects.js
+++ structure/scripts/homepage-fetch-projects.js
@@ -1,24 +1,18 @@
-
-projectTitle = document.getElementById("project-title");
-projectText = document.getElementById("project-text");
-
-const showDescription = (title, txt) => {
-    
-    console.log(title,txt)
-    projectTitle.innerHTML = title;
-    projectText.innerHTML = txt;
-    
-
-}
-
+//Fetch to api to get title and description for project card section
 const load = async () => {
     const promise = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await promise.json();
     console.log(data)
     const newData = data.slice(0,3);
     console.log(newData);
+    for (let i = 0; i < newData.length; i++) {
+        const element = newData[i];
+        let title = element.title;
+        let txt = element.body;
+        let projectTitle = document.getElementById("project-title-"+ i);
+        let projectText = document.getElementById("project-text-"+ i);
+        projectTitle.innerHTML = title;
+        projectText.innerHTML = txt;
+    }
     
-    let title = data[4].title
-    let txt = data[4].body
-    showDescription(title, txt)
 }
