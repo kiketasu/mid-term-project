f0b9b1c5147d037f88e86c869d129b4040ed59bd
Enric Orillosa <enricorillosa@hotmail.es>
2022-10-06 14:09:17 +0200

    Updated 16
    Hamburger menu fixed and functioning in all pages

--- structure/scripts/homepage-app.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-    let x = document.getElementsByClassName("navbar-brand");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  }
