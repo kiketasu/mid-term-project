e36388307806361b429abf692fa09626afa8f923
Enric <enricorillosa@hotmail.es>
2022-10-06 23:04:27 +0200

    Finished
    es lo que hay

--- structure/scripts/project-page.js
+++ structure/scripts/project-page.js
@@ -3,11 +3,11 @@ let projectTitle = document.getElementById("project-page-title");
 let projectText = document.getElementById("project-page-text");
 
 const load = async () => {
-const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await promise.json();
   console.log(data);
-  let title = data[0].title
-  let txt = data[1].body
+  let title = data[0].title;
+  let txt = data[1].body;
   projectTitle.innerHTML = title;
   projectText.innerHTML = txt;
-}
+};
