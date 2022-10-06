5ae7c403c2302e9cb62cbb165441c1df09cd218f
Enric <enricorillosa@hotmail.es>
2022-10-06 18:13:10 +0200

    Updated 18
    Fetch problems with homepage projects sections still working on it

--- structure/scripts/homepage-fetch-projects.js
+++ structure/scripts/homepage-fetch-projects.js
@@ -1,20 +1,24 @@
 
-projectTitle = document.getElementsByClassName("project-text-title");
-projectText = document.getElementsByClassName("project-text-description");
+projectTitle = document.getElementById("project-title");
+projectText = document.getElementById("project-text");
 
 const showDescription = (title, txt) => {
+    
+    console.log(title,txt)
     projectTitle.innerHTML = title;
     projectText.innerHTML = txt;
     
 
 }
 
-const load = async (e) => {
-    e.preventDefault();
+const load = async () => {
     const promise = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await promise.json();
-    let title = data.title
-    let txt = data.body
-
+    console.log(data)
+    const newData = data.slice(0,3);
+    console.log(newData);
+    
+    let title = data[4].title
+    let txt = data[4].body
     showDescription(title, txt)
 }
