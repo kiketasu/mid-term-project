85d0962d008f76945668702892c25a25dd78e425
Enric <enricorillosa@hotmail.es>
2022-10-06 21:45:26 +0200

    Updated 20
    Project page fetch implemented

--- structure/scripts/homepage-fetch-projects.js
+++ structure/scripts/homepage-fetch-projects.js
@@ -1,18 +1,17 @@
 //Fetch to api to get title and description for project card section
 const load = async () => {
-    const promise = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await promise.json();
-    console.log(data)
-    const newData = data.slice(0,3);
-    console.log(newData);
-    for (let i = 0; i < newData.length; i++) {
-        const element = newData[i];
-        let title = element.title;
-        let txt = element.body;
-        let projectTitle = document.getElementById("project-title-"+ i);
-        let projectText = document.getElementById("project-text-"+ i);
-        projectTitle.innerHTML = title;
-        projectText.innerHTML = txt;
-    }
-    
-}
+  const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await promise.json();
+  console.log(data);
+  const newData = data.slice(0, 3);
+  console.log(newData);
+  for (let i = 0; i < newData.length; i++) {
+    const element = newData[i];
+    let title = element.title;
+    let txt = element.body;
+    let projectTitle = document.getElementById("project-title-" + i);
+    let projectText = document.getElementById("project-text-" + i);
+    projectTitle.innerHTML = title;
+    projectText.innerHTML = txt;
+  }
+};
---
+++ structure/scripts/project-page.js
@@ -0,0 +1,13 @@
+//Fetch to api to get title and description for the Project card
+let projectTitle = document.getElementById("project-page-title");
+let projectText = document.getElementById("project-page-text");
+
+const load = async () => {
+const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await promise.json();
+  console.log(data);
+  let title = data[0].title
+  let txt = data[1].body
+  projectTitle.innerHTML = title;
+  projectText.innerHTML = txt;
+}
